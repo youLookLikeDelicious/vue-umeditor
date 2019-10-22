eba06fa523826e845e5371d476cefd73585c371d
youLookLikeDelicious <1059366817@qq.com>
2019-10-22 23:34:03 +0800

    remove .idea folder

---
+++ src/static/plugins/umeditor/third-party/indexOf.js
@@ -0,0 +1,17 @@
+// 修复IE8下 Array 不支持indexOf方法
+(function(){
+	if(Array.prototype.indexOf === undefined){
+		Array.prototype.indexOf = function(obj){
+			if(Object.prototype.toString(this) !== '[object Array]'){
+				return -1;
+			}
+			var pos = 0, len = this.length;
+			for(; pos < len; pos++){
+				if(this[pos] === obj)
+					return pos;
+			}
+			
+			return pos == len? -1 : pos;
+		};
+	}
+})();
